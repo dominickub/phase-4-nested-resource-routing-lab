f1e41e900cdce0d5a4dfaca6db8c034a59d581a7
dominick <dominickugas@gmail.com>
2021-12-16 11:53:03 -0500

    commit tree 0aa9b5efa928a9c498c564286365a1061d81ad68

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,40 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items= user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    if params[:user_id] 
+      user = User.find(params[:user_id])
+      items = user.items.find(params[:id])
+    else
+      items = Item.find(params[:id])
+    end
+    render json: items
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    items = user.items.create(item_params)
+    render json: items, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response
+    render json: { error: "404" }, status: :not_found
 end
+
+end
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,28 @@
 class UsersController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+  end
+
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def create
+    users = User.create(users_params)
+    render json: users
+  end
+
+  private
+
+  def users_params
+    params.permit(:username, :city)
+  end
+
+  def render_not_found_response
+    render json: { error: "404" }, status: :not_found
   end
 
 end
